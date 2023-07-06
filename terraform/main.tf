789f53c090fc23462199ad63804c9bba1d04bf4f
milor4668 <milor4668@gmail.com>
2023-07-06 15:56:43 +0200

    debut integration vault

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
 module "spVault" {
-    source = "./modules/spVault"
+  source = "./modules/vault"
+  vault_username_default = var.vault_username_default
+  vault_password_default = var.vault_password_default
 }
