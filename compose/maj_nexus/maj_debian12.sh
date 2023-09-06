329c2fd03cb31b062a07d70bcd7a07572c6e3c61
milor4668 <milor4668@gmail.com>
2023-09-06 17:40:15 +0200

    Ajout compose

--- compose/maj_nexus/maj_debian12.sh
+++ compose/maj_nexus/maj_debian12.sh
@@ -5,4 +5,7 @@ PORT="8081"
 USERNAME="admin"
 PASSWORD="q3urmyZiZKXSKtM"
 
-curl -u ${USERNAME}:${PASSWORD} -X POST "http://${IP}:${PORT}/service/rest/v1/repositories/apt/proxy" -H "Content-Type: application/json" -d @apt-debian.json
+curl -u ${USERNAME}:${PASSWORD} -X POST "http://${IP}:${PORT}/service/rest/v1/repositories/apt/proxy" -H "Content-Type: application/json" -d @create_debian.json
+
+#curl -u ${USERNAME}:${PASSWORD} -X PUT "http://${IP}:${PORT}/service/rest/v1/repositories/apt/proxy" -H "Content-Type: application/json" -d @update_debian.json
+
